de842d159db21efd29d5bcdd4295db22e4b67169
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-30 15:02:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,36 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index}. #{song}"
+  end
+end
+
+def play(songs)
+  songOutput = ""
+  puts "Please enter a song name or number:"
+  userInput = gets.chomp
+    songs.each_with_index do |song, index|
+      if song == userInput.to_s || index == userInput.to_i
+        songOutput = song
+      end
+  end
+  if songOutput.empty?
+    "Invalid input, please try again"
+  else
+    "Playing #{songOutput}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
